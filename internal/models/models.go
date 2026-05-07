fe7cbf9026c025f5c1166f58c5af1d00901eaed2
P0m32Kun <p0m32kun@gmail.com>
2026-05-07 19:53:47 +0800

    autocommit: 2026-05-07_19:53:47

--- internal/models/models.go
+++ internal/models/models.go
@@ -17,8 +17,6 @@ type Project struct {
 	RateLimit      int       `json:"rate_limit" db:"rate_limit"`
 	PortRange      *string   `json:"port_range,omitempty" db:"port_range"`
 	DefaultProfile string    `json:"default_profile" db:"default_profile"`
-	FofaEmail      *string   `json:"fofa_email,omitempty" db:"fofa_email"`
-	FofaAPIKey     *string   `json:"fofa_api_key,omitempty" db:"fofa_api_key"`
 	PipelineConfig *string   `json:"pipeline_config,omitempty" db:"pipeline_config"`
 	CreatedAt      time.Time `json:"created_at" db:"created_at"`
 	UpdatedAt      time.Time `json:"updated_at" db:"updated_at"`
