9e4c3639a0b46c512bba291eacf91a3d6885f1b8
P0m32Kun <p0m32kun@gmail.com>
2026-05-07 16:40:59 +0800

    autocommit: 2026-05-07_16:40:59

--- internal/workflow/pipeline.go
+++ internal/workflow/pipeline.go
@@ -886,7 +886,7 @@ func (p *Pipeline) runNucleiWeb(ctx context.Context, endpoints []*models.WebEndp
 			targetFile = abs
 		}
 
-		task, stdout, err := p.createAndRunTask(ctx, "nuclei", worker.BuildNucleiCommand(targetFile, "deep", p.config.NucleiRateLimit, tags, p.config.NucleiScanDepth, DefaultWorkflowDir))
+		task, stdout, err := p.createAndRunTask(ctx, "nuclei", worker.BuildNucleiCommand(targetFile, "deep", p.config.NucleiRateLimit, p.config.NucleiRateLimitPerMinute, p.config.NucleiConcurrency, tags, p.config.NucleiScanDepth, DefaultWorkflowDir))
 		if err != nil {
 			log.Printf("nuclei task for tags %s: %v", tagKey, err)
 			continue
