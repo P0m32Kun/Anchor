0dbcede4b93d43de1715679a8a8fc332a946ef37
P0m32Kun <p0m32kun@gmail.com>
2026-05-08 15:58:57 +0800

    autocommit: 2026-05-08_15:58:57

--- frontend/src/pages/FindingsPage.tsx
+++ frontend/src/pages/FindingsPage.tsx
@@ -2,6 +2,7 @@ import { useEffect, useMemo, useState } from "react";
 import { Link } from "react-router-dom";
 import { api, PAGE_ALL } from "../lib/api";
 import { useStore } from "../lib/store";
+import { useResource } from "../hooks";
 import {
   EmptyState,
   Input,
