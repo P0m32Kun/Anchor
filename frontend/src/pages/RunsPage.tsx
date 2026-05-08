08954b538e2e95cfd3d2d0403562abceac5b9435
P0m32Kun <p0m32kun@gmail.com>
2026-05-08 15:45:18 +0800

    autocommit: 2026-05-08_15:45:18

--- frontend/src/pages/RunsPage.tsx
+++ frontend/src/pages/RunsPage.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState, useCallback, useRef } from "react";
-import { useNavigate } from "react-router-dom";
 import { api, PAGE_ALL } from "../lib/api";
 import { useStore } from "../lib/store";
 import { 
