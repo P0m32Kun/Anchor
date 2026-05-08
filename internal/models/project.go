aa62b282cfb8f52c312f8df4936f9f441f97394e
P0m32Kun <p0m32kun@gmail.com>
2026-05-08 15:41:38 +0800

    autocommit: 2026-05-08_15:41:38

---
+++ internal/models/project.go
@@ -0,0 +1,18 @@
+package models
+
+import "time"
+
+// --- Project ---
+
+type Project struct {
+	ID             string    `json:"id" db:"id"`
+	Name           string    `json:"name" db:"name"`
+	Organization   string    `json:"organization" db:"organization"`
+	Purpose        string    `json:"purpose" db:"purpose"`
+	RateLimit      int       `json:"rate_limit" db:"rate_limit"`
+	PortRange      *string   `json:"port_range,omitempty" db:"port_range"`
+	DefaultProfile string    `json:"default_profile" db:"default_profile"`
+	PipelineConfig *string   `json:"pipeline_config,omitempty" db:"pipeline_config"`
+	CreatedAt      time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at" db:"updated_at"`
+}
