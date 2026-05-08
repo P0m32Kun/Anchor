a7ba95e22f61c0a5a1f539ab5f299798a0bfd731
P0m32Kun <p0m32kun@gmail.com>
2026-05-08 10:54:21 +0800

    autocommit: 2026-05-08_10:54:21

--- internal/db/db.go
+++ internal/db/db.go
@@ -1166,3 +1166,17 @@ func migrateV12(db *sql.DB) error {
 
 	return nil
 }
+
+func migrateV13(db *sql.DB) error {
+	var hasCol int
+	if err := db.QueryRow(`SELECT COUNT(*) FROM pragma_table_info('scan_tasks') WHERE name = 'error_message'`).Scan(&hasCol); err != nil {
+		return fmt.Errorf("check error_message column: %w", err)
+	}
+	if hasCol > 0 {
+		return nil
+	}
+	if _, err := db.Exec(`ALTER TABLE scan_tasks ADD COLUMN error_message TEXT`); err != nil {
+		return fmt.Errorf("add error_message column: %w", err)
+	}
+	return nil
+}
